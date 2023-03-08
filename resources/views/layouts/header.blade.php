55aae9a8de945523ee9527b3714d9ed24d6bef82
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-03-08 04:50:56 +0330

    implement layouts.master blade file

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+		<title>Laravel Payment System - @yield('title')</title>
+		<meta charset="UTF-8">
+		<meta name="language" content="en">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="robots" content="index, follow">
+		<meta name="designer" content="Nima jahan bakhshian/dvlpr1996">
+		<meta name="owner" content="Nima jahan bakhshian/dvlpr1996">
+		<meta name="author" content="Nima jahan bakhshian/dvlpr1996">
+		<meta name="keywords" content="Laravel Payment System, dvlpr1996,laravel,php,alpine.js,tailwind">
+		<meta name="description" content="Laravel Payment System">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="theme-color" content="#1C1D1F">
+		<meta name="msapplication-TileColor" content="#1C1D1F">
+		<meta name="msapplication-navbutton-color" content="#1C1D1F">
+		<meta name="apple-mobile-web-app-status-bar-style" content="#1C1D1F">
+		<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/pngx-icon">
+		<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.ico') }}">
+		<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.ico') }}">
+		<link rel="apple-touch-icon" sizes="180x180"href="{{ asset('favicon.ico') }}">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+		@vite(['resources/css/app.css','resources/js/app.js'])
+		<!-- [if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<![endif] -->
+</head>
+
+<body x-data="{ 'darkMode': true }" x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
+$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)));" class="max-w-7xl mx-auto px-4">
+
+		<div x-bind:class="{ 'dark': darkMode === true }">
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,4 @@
+@include('layouts.header')
+@include('layouts.nav')
+@yield('main')
+@include('layouts.footer')
