62311b0b81c3f52e9dbbcd74832379539b01b25f
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-03-16 13:05:17 +0330

    fix ProductSeeder issue

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -10,6 +11,26 @@ class ProductSeeder extends Seeder
 {
     public function run(): void
     {
-        Product::factory()->count(5)->create();
+        $titles = [
+            'Action collection',
+            'Comedy collection',
+            'Fantasy collection',
+            'Drama collection',
+            'Mystery collection',
+            'Romance collection',
+            'Horror collection',
+            'Sci-Fi collection'
+        ];
+
+        foreach ($titles as $title) {
+            Product::create([
+                'title' => $title,
+                'description' => 'type of label a piece of printed paper',
+                'price' => fake()->numberBetween(10000, 50000),
+                'slug' => Str::slug($title),
+                'stock' => fake()->numberBetween(0, 10),
+                'image' => 'https://loremflickr.com/5000/3334/world?random=' . fake()->randomNumber()
+            ]);
+        }
     }
 }
