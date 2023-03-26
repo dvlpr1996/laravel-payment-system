fb9f765ca7036f02fed6f9a64b73d885486edfe7
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-03-26 22:46:09 +0330

    add route global constraints

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,6 +34,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+
+        // Route Global Constraints
+        Route::pattern('product:slug', '[-a-zA-Z]+');
+        Route::pattern('product:id', '[0-9]+');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,18 @@ use App\Http\Controllers\BasketController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', [HomeController::class, 'index'])->name('index');
-Route::get('/checkout', [ProductController::class, 'index'])->name('checkout.index');
-Route::get('/product/{product:slug}', [ProductController::class, 'product'])->name('product');
-Route::get('/basket/add/{product:slug}', [BasketController::class, 'add'])->name('basket.add');
-Route::get('/basket/remove/{product}', [BasketController::class, 'remove'])
-    ->name('basket.remove');
-Route::get('/basket/update/{product}', [BasketController::class, 'update'])
-    ->name('basket.update');
-Route::get('/basket', [BasketController::class, 'index'])->name('basket.index');
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/checkout', 'index')->name('checkout.index');
+    Route::get('/product/{product:slug}', 'product')->name('product');
+});
+
+Route::controller(BasketController::class)->group(function () {
+    Route::get('/basket', 'index')->name('basket.index');
+    Route::get('/basket/add/{product:slug}', 'add')->name('basket.add');
+    Route::get('/basket/update/{product}', 'update')->name('basket.update');
+    Route::get('/basket/remove/{product}', 'remove')->name('basket.remove');
+});
 
 Route::view('/checkout ', 'app.checkout');
 
